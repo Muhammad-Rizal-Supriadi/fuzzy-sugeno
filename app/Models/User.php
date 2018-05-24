0baac434a78907999b5d9e5ee2c677d7ce907245
faridzy <faridzy09@gmail.com>
2018-05-25 01:31:43 +0700

    fixing logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,23 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: mfarid
- * Date: 07/05/18
- * Time: 20.35
+ * Date: 25/05/18
+ * Time: 01.28
  */
 
 namespace App\Models;
+
+
 
 
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    protected $table = 'users';
-    protected $primaryKey = 'id';
-    public $timestamps = false;
-
-    public function getRole(){
-        return $this->hasOne('App\Models\Role','id','role_id');
-    }
+    protected $table='users';
+    protected $primaryKey='id';
+    public $timestamps=false;
 
 }
